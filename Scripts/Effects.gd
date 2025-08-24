89fe9e052ac9ebe43cd374aa209530a221bf76a3
Trigware <angryhonzik@gmail.com>
2025-08-24 13:35:13 +0200

    Finished effect prototype

--- Scripts/Effects.gd
+++ Scripts/Effects.gd
@@ -9,10 +9,19 @@ enum Effect {
 }
 
 func _ready():
-	add_effect(Effect.Poison, 5)
+	update_effect(Effect.Poison, 5)
+	await get_tree().create_timer(2).timeout
+	update_effect(Effect.Poison, 2)
+	update_effect(Effect.Burning, 2)
 
 func _process(delta: float):
 	update_effect_durations(delta)
+
+func has_effect(effect: Effect):
+	return effect in ongoing_effects
+
+func get_effect_count():
+	return ongoing_effects.size()
 
 func update_effect_durations(delta):
 	for effect in ongoing_effects.keys():
@@ -20,19 +29,26 @@ func update_effect_durations(delta):
 		var updated_duration = current_duration - delta
 		ongoing_effects[effect] = updated_duration
 		var effect_instance = effects_scenes[effect]
+		
 		var rounded_time = ceili(updated_duration)
 		if updated_duration <= 0:
-			effect_instance.queue_free()
-			effects_scenes.erase(effect)
-			ongoing_effects.erase(effect)
-		effect_instance.time.text = str(rounded_time)
-	
-func add_effect(effect: Effect, duration: float):
-	ongoing_effects[effect] = duration
+			end_effect(effect_instance, effect)
+		else: effect_instance.time.text = str(rounded_time)
+
+func end_effect(effects_instance, effect: Effect):
+	effects_instance.queue_free()
+	effects_scenes.erase(effect)
+	ongoing_effects.erase(effect)
+
+func update_effect(effect: Effect, update_duration_by: float):
+	if effect in ongoing_effects:
+		ongoing_effects[effect] += update_duration_by
+		return
+	ongoing_effects[effect] = update_duration_by
 	var effect_scene = load("res://Scenes/EffectIcon.tscn")
 	var effect_instance = effect_scene.instantiate()
-	effect_instance.icon = get_effect_icon(effect)
 	add_child(effect_instance)
+	effect_instance.set_icon_texture(get_effect_icon(effect))
 	effects_scenes[effect] = effect_instance
 
 func get_effect_icon(effect: Effect):
--- Scripts/EffectsIcon.gd
+++ Scripts/EffectsIcon.gd
@@ -1,4 +1,12 @@
-extends Node
+extends Node2D
 
 @onready var icon = $Background/Icon
 @onready var time = $Time
+
+func set_icon_texture(texture):
+	icon.texture = texture
+
+const icon_x_offset = -150
+
+func _ready():
+	position.x += icon_x_offset * (Effects.get_effect_count() - 1)
