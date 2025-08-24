579ab973ad6c174f21f101b9a06edcb413309b52
Tomkoid <tomaszierl@outlook.com>
2025-08-24 20:43:03 +0200

    fix: center text immediately on resize
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- Scripts/Text.gd
+++ Scripts/Text.gd
@@ -15,6 +15,7 @@ func _ready():
 func update_text_pos():
 	var window_size = get_viewport().size
 	text_start_x = window_size.x / 2 - text_label.get_minimum_size().x / 2
+	text_label.position.x = text_start_x
 	text_hide_pos_y = window_size.y + 300
 	text_show_pos_y = window_size.y - 100
 
@@ -24,7 +25,6 @@ func change_text(new_text):
 const text_tween_duration = 0.4
 
 func text_tween(final_pos_y):
-	$Label.position.x = text_start_x
 	var text_move_tween = create_tween()
 	text_move_tween.tween_property(text_label, "position:y", final_pos_y, text_tween_duration)
 	text_move_tween.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
