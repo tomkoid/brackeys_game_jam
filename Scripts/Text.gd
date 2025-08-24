986c38fe35b58f693c159b7a1fbe61d0577e1455
Tomkoid <tomaszierl@outlook.com>
2025-08-24 20:36:27 +0200

    fix: adjust text position based on window size
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- Scripts/Text.gd
+++ Scripts/Text.gd
@@ -2,8 +2,21 @@ extends CanvasLayer
 
 @onready var text_label = $Label
 
-const text_hide_pos_y = 700
-const text_show_pos_y = 500
+var text_start_x: int
+var text_hide_pos_y = 700
+var text_show_pos_y = 500
+
+func _ready():
+	update_text_pos()
+	
+	text_label.position.y = text_hide_pos_y
+	get_viewport().size_changed.connect(update_text_pos)
+
+func update_text_pos():
+	var window_size = get_viewport().size
+	text_start_x = window_size.x / 2 - text_label.get_minimum_size().x / 2
+	text_hide_pos_y = window_size.y + 300
+	text_show_pos_y = window_size.y - 100
 
 func change_text(new_text):
 	text_label.text = new_text
@@ -11,6 +24,7 @@ func change_text(new_text):
 const text_tween_duration = 0.4
 
 func text_tween(final_pos_y):
+	$Label.position.x = text_start_x
 	var text_move_tween = create_tween()
 	text_move_tween.tween_property(text_label, "position:y", final_pos_y, text_tween_duration)
 	text_move_tween.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
