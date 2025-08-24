6064164874f33e90c84b1988cd305d5bd319efc2
Trigware <angryhonzik@gmail.com>
2025-08-24 15:37:28 +0200

    Added money HUD

---
+++ Scripts/HUD.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+@onready var money_label = $"Money Bag/Money Count"
+
+var money_count := 0
+
+func set_money(new_value):
+	money_count = new_value
+	money_label.text = str(money_count)
+
+func update_money(change_by):
+	set_money(money_count + change_by)
