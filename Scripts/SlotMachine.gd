f767f71ea6a2cb8318b7b66ca859f7b1b06d417e
Trigware <angryhonzik@gmail.com>
2025-08-24 20:26:55 +0200

    Camera zooms when interacting with the slot machine

--- Scripts/SlotMachine.gd
+++ Scripts/SlotMachine.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 @onready var interact_area = $"Interact Area"
+@onready var sprite = $Sprite
 
 const overlay_tween_duration = 0.4
 const overlay_show_alpha = 0.4
+var interact_disabled = false
 
 func _on_enter_interaction_area(body: Node2D) -> void:
 	if not body.is_in_group("Player"): return
@@ -17,13 +19,24 @@ func _on_exit_interaction_area(body: Node2D) -> void:
 
 func _unhandled_input(_event):
 	if Input.is_action_just_pressed("interact") and can_interact():
-		print("INTERACTS WITH SLOT MACHINE")
-		$Sprite.play("button")
-		await get_tree().create_timer(1.0).timeout
-		get_tree().change_scene_to_file("res://Scenes/Gamble.tscn")
-		pass
+		interact_with_slot_machine()
+
+const final_zoom := Vector2(3, 3)
+const camera_zoom_tween := 0.5
+
+func interact_with_slot_machine():
+	sprite.play("button")
+	interact_disabled = true
+	Player.camera.global_position = global_position
+	Text.hide_text()
+	var zoom_tween = create_tween().tween_property(Player.camera, "zoom", final_zoom, camera_zoom_tween)
+	zoom_tween.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE).finished
+	await Overlay.overlay_tween(1)
+	Player.physics_disabled = true
+	get_tree().change_scene_to_file("res://Scenes/Gamble.tscn")
 
 func can_interact():
+	if interact_disabled: return false
 	var overlapping_bodies = interact_area.get_overlapping_bodies()
 	for body in overlapping_bodies:
 		if body.is_in_group("Player"): return true
